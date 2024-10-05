545eac1d50bec8c9b92961ce5f63f821ce1a19d7
Jan Hähne <github@jhae.de>
2024-10-05 11:59:04 +0200

    Allow @import rule in CSS files

--- tests/css/at-rule-disallowed-list.test.ts
+++ tests/css/at-rule-disallowed-list.test.ts
@@ -5,6 +5,15 @@ RuleTest.setConfigFile('index.yaml');
 RuleTest.describe(
   'at-rule-disallowed-list',
   {
+    name: 'Disallow @debug rule',
+    code: '@debug "Debug";',
+    expect: {
+      errored: true,
+      messages: ['Unexpected at-rule "debug"'],
+      severities: ['error'],
+    },
+  },
+  {
     name: 'Disallow @import rule',
     code: '@import "test.css";',
     expect: {
@@ -14,12 +23,7 @@ RuleTest.describe(
     },
   },
   {
-    name: 'Disallow @debug rule',
-    code: '@debug "Debug";',
-    expect: {
-      errored: true,
-      messages: ['Unexpected at-rule "debug"'],
-      severities: ['error'],
-    },
+    name: 'Allow @import rule in CSS files',
+    files: './tests/.resources/at-rule-disallowed-list.css',
   },
 );
