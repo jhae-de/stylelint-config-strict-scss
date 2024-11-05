3548b4a5ea26532c7725f7f28da02ef3b76fa1f0
Jan Hähne <github@jhae.de>
2024-11-05 01:36:23 +0100

    Disallow value none for outline property

--- tests/css/declaration-property-value-disallowed-list.test.ts
+++ tests/css/declaration-property-value-disallowed-list.test.ts
@@ -3,26 +3,44 @@ import { type Severity } from 'stylelint';
 
 RuleTest.setConfigFile('index.yaml');
 
-RuleTest.describe('declaration-property-value-disallowed-list', {
-  name: 'Disallow value "none" for "border" properties',
-  code: `
-    test {
-      border: none;
-      border-top: none;
-      border-right: none;
-      border-bottom: none;
-      border-left: none;
-    }
-  `,
-  expect: {
-    errored: true,
-    messages: [
-      'Unexpected value "none" for property "border"',
-      'Unexpected value "none" for property "border-top"',
-      'Unexpected value "none" for property "border-right"',
-      'Unexpected value "none" for property "border-bottom"',
-      'Unexpected value "none" for property "border-left"',
-    ],
-    severities: new Array(5).fill('error') as Severity[],
+RuleTest.describe(
+  'declaration-property-value-disallowed-list',
+  {
+    name: 'Disallow value "none" for "border" and "outline" properties',
+    code: `
+      test {
+        border: none;
+        border-top: none;
+        border-right: none;
+        border-bottom: none;
+        border-left: none;
+        outline: none;
+      }
+    `,
+    expect: {
+      errored: true,
+      messages: [
+        'Unexpected value "none" for property "border"',
+        'Unexpected value "none" for property "border-top"',
+        'Unexpected value "none" for property "border-right"',
+        'Unexpected value "none" for property "border-bottom"',
+        'Unexpected value "none" for property "border-left"',
+        'Unexpected value "none" for property "outline"',
+      ],
+      severities: new Array(6).fill('error') as Severity[],
+    },
   },
-});
+  {
+    name: 'Allow value "0 none" for "border" and "outline" properties',
+    code: `
+      test {
+        border: 0 none;
+        border-top: 0 none;
+        border-right: 0 none;
+        border-bottom: 0 none;
+        border-left: 0 none;
+        outline: 0 none;
+      }
+    `,
+  },
+);
