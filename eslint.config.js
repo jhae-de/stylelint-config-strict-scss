8151140f9b380dc959cd32ba5c3d940feae7cbc0
Jan Hähne <github@jhae.de>
2025-01-18 14:29:39 +0100

    Update ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,14 @@
 import eslint from '@eslint/js';
 import prettier from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
 import tslint from 'typescript-eslint';
 
 export default tslint.config(
   {
     extends: [eslint.configs.recommended, ...tslint.configs.recommendedTypeChecked, prettier],
     languageOptions: {
-      ecmaVersion: 2022,
-      globals: globals.node,
       parserOptions: {
-        project: 'tsconfig.eslint.json',
+        projectService: true,
       },
-      sourceType: 'module',
     },
   },
   {
