58de09ebb54c282fd5412c1fa26e33d1ba760134
Jan Hähne <github@jhae.de>
2025-09-20 13:11:32 +0200

    Update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,15 @@
 import eslint from '@eslint/js';
-import prettier from 'eslint-plugin-prettier/recommended';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig } from 'eslint/config';
 import tslint from 'typescript-eslint';
 
-export default tslint.config(
-  {
-    extends: [eslint.configs.recommended, ...tslint.configs.recommendedTypeChecked, prettier],
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
+export default defineConfig({
+  extends: [eslint.configs.recommended, ...tslint.configs.recommendedTypeChecked, prettierRecommended],
+  languageOptions: {
+    parserOptions: {
+      projectService: {
+        allowDefaultProject: ['*.js'],
       },
     },
   },
-  {
-    files: ['**/*.{js,cjs,mjs}'],
-    extends: [tslint.configs.disableTypeChecked],
-  },
-);
+});
